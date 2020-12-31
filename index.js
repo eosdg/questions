2d3458ac9e8577a10e9a7c03de6e78ddf6eebd49
neferin12 <julian.pollinger@jp-studios.de>
2020-12-31 21:52:09 +0100

    Mehr Fragetypen und Fragen

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import unbedenklich from "./questions/unbedenklich.json"
+import anzueglich from "./questions/anzueglich.json"
+import fsk18 from "./questions/fsk18.json"
+import peinlich from "./questions/peinlich.json"
 
 export default {
-    unbedenklich
+    unbedenklich, anzueglich, fsk18, peinlich
 }
