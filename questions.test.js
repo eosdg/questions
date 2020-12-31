a7d665668324db9c25ba6d09ce55cbaed589a83c
neferin12 <julian.pollinger@jp-studios.de>
2020-12-31 20:44:59 +0100

    Schema in eine Datei und tests

---
+++ questions.test.js
@@ -0,0 +1,13 @@
+import questions from "./index.js"
+import {matchers, matchersWithOptions} from "jest-json-schema"
+import {expect} from '@jest/globals'
+import schema from "./schema/questions.schema.json"
+
+expect.extend(matchers);
+
+const questionSets = Object.keys(questions);
+for (const questionSet of questionSets) {
+    it("Testing \"" + questionSet+"\"", () => {
+        expect(questions[questionSet]).toMatchSchema(schema);
+    });
+}
