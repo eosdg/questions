9fa2e6b181243cfdf22f63bfccf0fca41acb0273
Julian Pollinger <neferin42@gmail.com>
2021-01-02 23:59:00 +0100

    Quizkategorie

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import unbedenklich from "./questions/unbedenklich.json"
 import anzueglich from "./questions/anzueglich.json"
 import fsk18 from "./questions/fsk18.json"
 import peinlich from "./questions/peinlich.json"
+import quiz from "./questions/quiz.json"
 
 export default {
-    unbedenklich, anzueglich, fsk18, peinlich
+    unbedenklich, anzueglich, fsk18, peinlich, quiz
 }
